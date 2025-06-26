0b0a22996b17da3019f762f07de93668a636ba06
MRodriguez-M <melissa.rodriguez.murguia@gmail.com>
2025-06-26 00:10:32 -0600

    Reformatted output, cleaned up code

--- StudentGrading/StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
+++ StudentGrading/StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c7f5eb7bf43b8872175b257f0ea20db174bcf12")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
